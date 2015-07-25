c6ddb562b41d973019d5e0c894391c033369122d
Brian Cohn <brian_cohn14@pitzer.edu>
2015-07-25 12:33:07 -0700

    added database and subscription/publication data

--- sense8/sense8.js
+++ sense8/sense8.js
@@ -1,7 +1,11 @@
+//init the database which will hold all of the time series data
+// Each sample is a JSON element that holds the sensor values at a given time.
+Samples = new Mongo.Collection("Samples");
+
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
-
+  Meteor.subscribe("Samples");
   Template.hello.helpers({
     counter: function () {
       return Session.get('counter');
@@ -17,18 +21,32 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
+   Meteor.publish("tasks", function () {
+    return Tasks.find({
+      $or: [
+        { private: {$ne: true} },
+        { owner: this.userId }
+      ]
+    });
+  });
   Meteor.startup(function () {
     // code to run on server at startup
   });
 }
 
-Router.route('/api', {where:'server'})
-.put(function(){
+Router.map(function () {
+  this.route('serverFile', {
+    path: '/receive/',
+    where: 'server',
 
-  var req;
-  req = this.request;
-  console.log(req.body);
-	//this is where we need to add it into the database.
-this.response.writeHead(200, {'Content-Type':'application/json; charset=utf-8'});
-  this.response.end('PUT finished.');
+    action: function () {
+      var filename = this.params.filename;
+      resp = {'lat' : this.request.body.lat,
+              'lon' : this.request.body.lon};
+      Samples.insert(this.request.body)
+      this.response.writeHead(200, {'Content-Type': 
+                                    'application/json; charset=utf-8'});
+      this.response.end(JSON.stringify(resp));
+    }
+  });
 });
